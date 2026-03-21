91808ecba9d208bfe6644b5bdd02d768344ee663
ANGADSINGHT <ANGADPALTAK@OUTLOOK.COM>
2026-03-21 19:53:09 +0000

    The beginning...

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+class Game:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+    
+    def compile(self):
+        # TODO: Check if story exists
+        
+        with open('story.txt','r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            points = line.split("|")
+
+if __name__ == "__main__":
+    game=Game()
+    game.compile('story.txt')
