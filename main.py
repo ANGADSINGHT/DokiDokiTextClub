b9aa375e32f1e0cff602e2f2153e24b16fe344cf
ANGADSINGHT <ANGADPALTAK@OUTLOOK.COM>
2026-03-22 19:32:11 +0000

    Basic compiling done

--- main.py
+++ main.py
@@ -2,23 +2,43 @@ import os
 
 
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.story = {}
+        self.name = name
+        self.npcnames = []
 
     def run(self):
-        pass
+        print(self.story)
 
     def compile(self, PATH="story.txt"):
         # TODO: Check if story exists
 
-        with open('story.txt', 'r') as f:
+        with open(PATH, 'r') as f:
             lines = f.readlines()
 
         for line in lines:
-            points = line.split("|")
-            print(points)
-
+            points = line.strip().split("|")
+            scene = 0
+            for point in points:
+                if len(point) > 0 or point == '':
+                    pass
+                elif point[0] == 'scene':
+                    scene = int(point[1])
+                    self.story[scene] = []
+                elif point[0] == '{player}':
+                    point[0] = self.name
+                elif point[0] == 'names':
+                    self.npcnames = point[1].split('/')
+                elif point[0] in self.npcnames:
+                    self.story[scene].append(f'[{point[0]}] {point[1]}')
+                elif point[0] == 'set':
+                    exec(f"{point[1]}={point[2]}")
+                elif point[0] == 'log':
+                    exec(f"print({point[1]})")
 
 if __name__ == "__main__":
-    game = Game()
+    # Persistent settings
+    name = input("What is your name? ")
+    game = Game(name)
     game.compile('story.txt')
+    game.run()
