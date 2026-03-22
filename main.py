0d81010a9670f97a4cc2988d7a3f4f50f9ebb5e7
ANGADSINGHT <angadpaltak@outlook.com>
2026-03-22 17:32:06 +0000

    Story layout

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import os
 
+
 class Game:
     def __init__(self):
         pass
-    
+
     def run(self):
         pass
-    
-    def compile(self):
+
+    def compile(self, PATH="story.txt"):
         # TODO: Check if story exists
-        
-        with open('story.txt','r') as f:
+
+        with open('story.txt', 'r') as f:
             lines = f.readlines()
 
         for line in lines:
             points = line.split("|")
+            print(points)
+
 
 if __name__ == "__main__":
-    game=Game()
+    game = Game()
     game.compile('story.txt')
